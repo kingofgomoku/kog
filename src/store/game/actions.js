a7e0944ff140818e452c64bcd4379617c8c53bdb
Yikern <yikernlooi@gmail.com>
2021-01-26 00:16:47 -0500

    added - board gomoku winner logic

--- src/store/game/actions.js
+++ src/store/game/actions.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-
 const actions = {
   gameInitializeGameBoard(context, size) {
     context.commit('gameClearGameBoard', size);
@@ -11,6 +10,85 @@ const actions = {
   },
   gameUpdateGameBoard(context, move) {
     context.commit('gameUpdateGameBoard', move);
+    context.dispatch('gameCheckGameWinner');
+  },
+  gameCheckGameWinner(context) {
+    let formattedString = context.state.gameboard.map(rows => {
+      return rows.map(col => {
+        if ( 
+          Object.keys(col).length === 0 && 
+          col.constructor === Object 
+        ) {
+          return 'x'
+        } else {
+          return col.color.slice(0,1)
+        }
+      }).join('')
+    })
+    let transposed = context.state.gameboard[0].map((col,i) => context.state.gameboard.map(row => row[i]))
+    let transposedFormattedString = transposed.map(rows => {
+      return rows.map(col => {
+        if ( 
+          Object.keys(col).length === 0 && 
+          col.constructor === Object 
+        ) {
+          return 'x'
+        } else {
+          return col.color.slice(0,1)
+        }
+      }).join('')
+    })
+    context.dispatch('gameCheckHorVerLineWinner', formattedString);
+    context.dispatch('gameCheckHorVerLineWinner', transposedFormattedString);
+    context.dispatch('gameCheckDiagLineWinner', {
+      arrayString: formattedString,
+      color: 'w'
+    });
+    context.dispatch('gameCheckDiagLineWinner', {
+      arrayString: formattedString,
+      color: 'b'
+    });
+  },
+  gameCheckHorVerLineWinner(context, arrayString) {
+    let superString = arrayString.join('x');
+    if(/(b{5,5})/.test(superString)) {
+      console.log('black Wins')
+      // use context to update
+    }
+    if (/(w{5,5})/.test(superString)) {
+      console.log('white Wins')
+      // use context to update
+    }
+  },
+  gameCheckDiagLineWinner(context, {arrayString, color}) {
+    for (let i = 0; i < arrayString.length - 4 ; i++) {
+      for(let j = 0; j < arrayString[i].length - 4 ; j++) {
+        if(
+          arrayString[i].charAt(j) === color &&
+          arrayString[i+1].charAt(j+1) === color &&
+          arrayString[i+2].charAt(j+2) === color &&
+          arrayString[i+3].charAt(j+3) === color &&
+          arrayString[i+4].charAt(j+4) === color 
+        ) {
+          //declare winner
+          console.log(color,' wins')
+        }
+      }
+    }
+    for (let i = 4; i < arrayString.length; i++) {
+      for(let j = 4; j < arrayString[i].length ; j++) {
+        if(
+          arrayString[i].charAt(j) === color &&
+          arrayString[i+1].charAt(j-1) === color &&
+          arrayString[i+2].charAt(j-2) === color &&
+          arrayString[i+3].charAt(j-3) === color &&
+          arrayString[i+4].charAt(j-4) === color 
+        ) {
+          //declare winner
+          console.log(color,' wins')
+        }
+      }
+    }
   }
 };
 
